8623c6727b726feae10176019a3b08d97edc9747
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-01 11:31:17 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Main from './views/Main';
 import Detail from './views/Detail';
 import Update from './views/Update';
 import Error from './views/Error';
 import Create from './views/Create';
+import RecipeNavigator from './components/RecipeNavigator'
+import RandomRestaurant from './components/RandomRestaurant';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const themeChanged = () => {
+    theme === 'dark' ? setTheme('light') : setTheme('dark');
+  }
+
   return (
-    <div className="App p-8 flex justify-center">
-      <Routes>
-        <Route path="/" element={<Navigate to="/foods"/>}/>
-        <Route element={<Main/>} path="/foods" />
-        <Route element={<Create/>} path="/foods/new" />
-        <Route element={<Detail/>} path="/foods/:id" />
-        <Route element={<Update/>} path="/foods/:id/edit" />
-        <Route element={<Error/>} path="*" />
-      </Routes>
+    <div data-theme={theme} className='h-screen min-h-max'>
+      <NavBar onThemeChange={ themeChanged }/>
+      <div className="py-24 flex justify-center">
+        <Routes>
+          <Route path="/" element={<Navigate to="/chowcounter/foods"/>}/>
+          <Route element={<RecipeNavigator/>} path="/chowcounter/recipes" />
+          <Route element={<RandomRestaurant/>} path="/chowcounter/restaurant-generator" />
+          <Route element={<Main/>} path="/chowcounter/foods" />
+          <Route element={<Create/>} path="/chowcounter/foods/new" />
+          <Route element={<Detail/>} path="/chowcounter/foods/:id" />
+          <Route element={<Update/>} path="/chowcounter/foods/:id/edit" />
+          <Route element={<Error/>} path="*" />
+        </Routes>
+      </div>
     </div>
   );
 }
