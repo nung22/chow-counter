112ce66ad062abcf47808877218eb6617844b2a5
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-02 23:29:20 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Main from './views/Main';
-import Detail from './views/CRUD/Detail';
-import Update from './views/CRUD/Update';
-import Create from './views/CRUD/Create';
+import Detail from './views/User/Detail';
+import Update from './views/User/Update';
+import Create from './views/User/Create';
 import RecipePicker from './views/RecipePicker'
 import NavBar from './components/NavBar';
 import RestaurantPicker from './views/RestaurantPicker';
@@ -21,14 +21,14 @@ function App() {
       <NavBar onThemeChange={ themeChanged }/>
       <div className="flex justify-center">
         <Routes>
-          <Route path="/" element={<Navigate to="/chowcounter/foods"/>}/>
+          <Route path="/" element={<Navigate to="/chowcounter/users"/>}/>
           <Route element={<RecipePicker/>} path="/chowcounter/recipes" />
           <Route element={<RestaurantPicker/>} path="/chowcounter/restaurants" />
           <Route element={<FoodSearch/>} path="/chowcounter/food-search" />
-          <Route element={<Main/>} path="/chowcounter/foods" />
-          <Route element={<Create/>} path="/chowcounter/foods/new" />
-          <Route element={<Detail/>} path="/chowcounter/foods/:id" />
-          <Route element={<Update/>} path="/chowcounter/foods/:id/edit" />
+          <Route element={<Main/>} path="/chowcounter/users" />
+          <Route element={<Create/>} path="/chowcounter/users/new" />
+          <Route element={<Detail/>} path="/chowcounter/users/:id" />
+          <Route element={<Update/>} path="/chowcounter/users/:id/edit" />
           <Route element={<Navigate to="/"/>} path="*" />
         </Routes>
       </div>
