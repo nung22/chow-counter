9eba1ae4da2d8d5d290036db3a942974bfe2a343
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-02 23:13:59 -0800

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,20 +1,70 @@
 const { User } = require("../models/user.model");
 const { 
-  createUser,
   getAllUsers,
   getUserById,
   deleteUserById,
   updateUserById,
 } = require("../services/user.service");
 
-const handleCreateUser = async (req, res) => {
-  try {
-    const newUser = await createUser(req.body);
-    return res.json(newUser);
-  } catch (err) {
-    return res.status(400).json(err);
-  }
+const handleRegisterUser = async (req, res) => {
+  User.create(req.body)
+    .then((user) => {
+      const userToken = jwt.sign(
+        {
+          id: user._id,
+        },
+        process.env.SECRET_KEY
+      );
+
+      res
+        .cookie("usertoken", userToken, secret, {
+          httpOnly: true,
+        })
+        .json({ msg: "success!", user: user });
+    })
+    .catch((err) => res.status(400).json(err));
 };
+
+const handleLoginUser = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (user === null) {
+    // email not found in users collection
+    return res.sendStatus(400);
+  }
+
+  // if we made it this far, we found a user with this email address
+  // let's compare the supplied password to the hashed password in the database
+  const correctPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (!correctPassword) {
+    // password wasn't a match!
+    return res.sendStatus(400);
+  }
+
+  // if we made it this far, the password was correct
+  const userToken = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.SECRET_KEY
+  );
+
+  // note that the response object allows chained calls to cookie and json
+  res
+    .cookie("usertoken", userToken, secret, {
+      httpOnly: true,
+    })
+    .json({ msg: "success!" });
+};
+
+const handleLogoutUser = (req, res) => {
+  res.clearCookie('usertoken');
+  res.sendStatus(200);
+}
 
 const handleGetAllUsers = async (req, res) => {
   try {
@@ -52,7 +102,9 @@ const handleUpdateUserById = async (req, res) => {
 };
 
 module.exports = {
-  handleCreateUser,
+  handleRegisterUser,
+  handleLoginUser,
+  handleLogoutUser,
   handleGetAllUsers,
   handleGetUserById,
   handleDeleteUserById,
