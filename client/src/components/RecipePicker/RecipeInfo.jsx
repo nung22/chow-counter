b21cf739e99bae02f97cd6847e164d5ffd5141ff
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-02-03 02:43:27 -0800

    Update RecipeInfo.jsx

--- client/src/components/RecipePicker/RecipeInfo.jsx
+++ client/src/components/RecipePicker/RecipeInfo.jsx
@@ -5,43 +5,45 @@ import noRecipes from "./assets/no-recipes.gif"
 
 export default function RecipeInfo(props) {
   const { tags, newSearch } = props;
+  const [numSearches, setNumSearches] = useState(0);
   const [recipe, setRecipe] = useState({});
   const [loaded, setLoaded] = useState(false);
-  const [noResults, setNoResults] = useState(true);
+  const [noResults, setNoResults] = useState(false);
 
-  // useEffect(() => {
-  //   const options = {
-  //     method: 'GET',
-  //     url: "http://localhost:8000/externalApi/random-recipe", 
-  //     params: {
-  //       tags: tags.join(",")
-  //     }
-  //   }
-  //   axios.request(options)
-  //     .then(response => {
-  //       if(response.data.recipes.length === 0) {
-  //         setRecipe({});
-  //         setNoResults(true);
-  //       } else {
-  //         console.log(response.data)
-  //         setNoResults(false);
-  //         const result = response.data.recipes[0];
-  //         setRecipe(result);
-  //         setLoaded(true);
-  //       }
-  //     })
-  //     .catch(err => {
-  //       console.error(err);
-  //     })
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: "http://localhost:8000/externalApi/random-recipe", 
+      params: {
+        tags: tags.join(",")
+      }
+    }
+    axios.request(options)
+      .then(response => {
+        if(response.data.recipes.length === 0) {
+          setRecipe({});
+          setNoResults(true);
+        } else {
+          console.log(response.data)
+          setNumSearches(numSearches + 1)
+          setNoResults(false);
+          const result = response.data.recipes[0];
+          setRecipe(result);
+          setLoaded(true);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      })
     
-  // // }, [newSearch]);
+  }, [newSearch]);
   // }, []);
 
   return (
     <div className="w-80 sm:w-96 sm:pt-12">
-      {loaded && Object.keys(recipe).length !== 0 && 
-        <div className="flex flex-col items-start gap-2 bg-base-300 rounded-2xl p-5">
-          <h1 className="text-3xl font-bold text-accent mb-3">{recipe.title}</h1>
+      {loaded && Object.keys(recipe).length !== 0 && numSearches > 1 && 
+        <div className="flex flex-col items-start gap-3 bg-base-300 rounded-2xl p-5">
+          <h1 className="text-2xl font-bold text-accent">{recipe.title}</h1>
           <div className="flex gap-2 flex-wrap">
             <div className="flex gap-2 flex-wrap">
               {recipe.cuisines !== undefined && recipe.cuisines.map( cuisine =>
@@ -64,9 +66,27 @@ export default function RecipeInfo(props) {
               )}
             </div>
           </div>
-          <img className="rounded-lg border-2 my-1" width={350} height={350} src={recipe.image} alt={recipe.title} />
-          <p className="font-semibold"><span className="font-bold text-secondary">Ready in:</span> {recipe.readyInMinutes} minutes</p>
-          <p className="font-semibold"><span className="font-bold text-secondary">Yield:</span> {recipe.readyInMinutes} minutes</p>
+          <img className="rounded-lg border-2" width={350} height={350} src={recipe.image} alt={recipe.title} />
+          <div>
+            <p className="font-semibold"><span className="font-bold text-info">Ready in -</span> {recipe.readyInMinutes} minutes</p>
+            <p className="font-semibold"><span className="font-bold text-info">Yield -</span> {recipe.servings} servings</p>
+          </div>
+          {/* <div>
+            <p className="font-bold text-warning underline">Ingredients</p>
+            <ul className="pl-3 grid grid-cols-2 gap-2 text-sm">
+              {recipe.extendedIngredients.map( (ingredient, i) =>
+                <li key={i} className="list-disc">{ingredient.original}</li>
+              )}
+            </ul>
+          </div>
+          <div>
+            <p className="font-bold text-warning underline">Instructions</p>
+            <ol className="pl-3">
+              {recipe.instructions.split(".").map( (line, i) =>
+                line.length > 0 ? <li key={i} className="list-decimal">{line}.</li> : ''
+              )}
+            </ol>
+          </div> */}
           <a href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer" className="btn btn-secondary text-lg">View Full Recipe</a>
         </div>
       }
